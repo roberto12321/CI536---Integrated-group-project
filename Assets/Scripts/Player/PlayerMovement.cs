0b8a3249754660a82cf672086759e0bcf6879c5a
roberto12321 <117285768+roberto12321@users.noreply.github.com>
2025-02-26 18:57:37 +0000

    v1.0.8

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public Transform cam;
 
 
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 3.0f;
+    private float velocity;
     public float speed = 6f;
+
+    private Vector3 moveDirection;
 
     public float turnSmoothTime = 0.1f;
 
@@ -21,11 +26,13 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 moveInput = move.action.ReadValue<Vector2>();
         Vector3 direction = new Vector3(moveInput.x , 0 , moveInput.y).normalized;
 
+        moveDirection = new Vector3(0,0,0);
+        moveDirection += new Vector3(0, ApplyGravity(),0);
 
         if (direction.magnitude >= 0.1f)
         {
@@ -33,8 +40,29 @@ public class PlayerMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            moveDirection += (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized * speed * Time.deltaTime;
+            
         }
+        print(moveDirection);
+        controller.Move(moveDirection);
+        
+        
+    }
+    private float ApplyGravity()
+    {
+        if (controller.isGrounded)
+        {
+            velocity = -1f;
+            print("Grounded: " + velocity);
+
+        }
+        else
+        {
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+            print("Airborne: " + velocity);
+
+        }
+        return velocity;
+        
     }
 }
